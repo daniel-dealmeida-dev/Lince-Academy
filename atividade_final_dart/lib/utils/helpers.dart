329d8369f2b5b1d5a786b23a437a519dd46c6fd1
Daniel de Almeida <daniel.dealmeida.dev@gmail.com>
2026-06-18 17:49:22 -0300

    refactor: Implementação dos métodos utilitarios do projeto

---
+++ atividade_final_dart/lib/utils/helpers.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+class Helpers {
+  //conversões
+  static double celciusParaFahrenheit(double c) => (c * 9 / 5) +32;
+  static double celciusParaKelvin(double c) => c + 273.15;
+  static double grausParaRadianos(double g) => g * pi / 100;
+
+  //formatações de datas
+  static String obterNomemes(int mes){
+    const meses = {
+      1: 'Janeiro',
+      2: 'Fevereiro',
+      3: 'Março',
+      4: 'Abril',
+      5: 'Maio',
+      6: 'Junho',
+      7: 'Julho',
+      8: 'Agosto',
+      9: 'Setembro',
+      10: 'Outubro',
+      11: 'Novembro',
+      12: 'Dezembro'
+    };
+    return meses[mes] ?? 'Desconhecido';
+  }
+
+  //funcção com um regex pra salvar um TXT
+  static String removerCodigoAnsi(String texto){
+    return texto.replaceAll(RegExp(r'\x1B\[[0-9;]*m'), '');
+  }
+
+}
