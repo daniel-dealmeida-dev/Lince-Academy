fec84e2fc1ba41deb31212be2bb7f7fec7e8708d
Daniel de Almeida <daniel.dealmeida.dev@gmail.com>
2026-06-18 21:11:20 -0300

    refactor: Implementação completa do services

--- atividade_final_dart/lib/services/processador_estatico.dart
+++ atividade_final_dart/lib/services/processador_estatico.dart
@@ -1,87 +1,101 @@
 class MetricasMensais {
   double tempSoma = 0.0;
   int tempQtd = 0;
-  double tempMax = -999.0;
-  double tempMin = 999.0;
+  double tempMax = double.negativeInfinity;
+  double tempMin = double.infinity;
 
   double umiSoma = 0.0;
   int umiQtd = 0;
-  double umiMax = -999.0;
-  double umiMin = 999.0;
+  double umiMax = double.negativeInfinity;
+  double umiMin = double.infinity;
 
-  // map pra contar a frequência das direções do vento com moda
+  // Frequência das direções do vento
   final Map<double, int> frequenciaVento = {};
 
-  void registrarLeitura(double temp, double umidade, double direcaoVento){
-    //Temperatura
+  void registrarLeitura(
+    double temp,
+    double umidade,
+    double vento,
+  ) {
+    // Temperatura
     tempSoma += temp;
     tempQtd++;
 
     if (temp > tempMax) tempMax = temp;
     if (temp < tempMin) tempMin = temp;
 
-    //Umidade
+    // Umidade
     umiSoma += umidade;
     umiQtd++;
-    if(umidade > umiMax) umiMax = umidade;
-    if(umidade < umiMin) umiMin = umidade;
 
+    if (umidade > umiMax) umiMax = umidade;
+    if (umidade < umiMin) umiMin = umidade;
 
-    //vento
-    frequenciaVento.update(direcaoVento,
-    (qtd)=> qtd + 1, 
-    ifAbsent: () => 1);
+    // Vento
+    frequenciaVento.update(
+      vento,
+      (qtd) => qtd + 1,
+      ifAbsent: () => 1,
+    );
   }
 
-  double  calcularTempMedia(){
+  double calcularTempMedia() {
     return tempQtd > 0 ? tempSoma / tempQtd : 0.0;
   }
 
-  double calcularUmiMedia(){
+  double calcularUmiMedia() {
     return umiQtd > 0 ? umiSoma / umiQtd : 0.0;
   }
 
-  double calcularModaVento(){
+  double calcularModaVento() {
     double direcaoMaisFrequente = 0.0;
     int maiorOcorrencia = 0;
-  
-    frequenciaVento.forEach((grau, qtd){
-      if(qtd > maiorOcorrencia){
+
+    frequenciaVento.forEach((grau, qtd) {
+      if (qtd > maiorOcorrencia) {
         maiorOcorrencia = qtd;
         direcaoMaisFrequente = grau;
       }
     });
+
     return direcaoMaisFrequente;
   }
 }
 
 class ProcessadorEstatistico {
-
   final Map<String, Map<int, Map<int, MetricasMensais>>> _dados = {};
 
   void registrarDadosNoMes({
     required String uf,
     required int ano,
     required int mes,
+    required double umidade,
     required double temp,
     required double vento,
-  }){
-    _dados.putIfAbsent(uf, ()=>{});
+  }) {
+    uf = uf.toUpperCase();
+
+    _dados.putIfAbsent(uf, () => {});
     final mapaDoEstado = _dados[uf]!;
 
-    mapaDoEstado.putIfAbsent(ano, ()=>{});
+    mapaDoEstado.putIfAbsent(ano, () => {});
     final mapaDoAno = mapaDoEstado[ano]!;
 
-
-    mapaDoAno.putIfAbsent(mes, ()=> MetricasMensais());
+    mapaDoAno.putIfAbsent(mes, () => MetricasMensais());
     final metricasDoMes = mapaDoAno[mes]!;
 
-    metricasDoMes.registrarLeitura(temp,  vento);
-    }
+    metricasDoMes.registrarLeitura(
+      temp,
+      umidade,
+      vento,
+    );
+  }
 
-    //TODO - terminar implementação do metodo estatistico
-    MetricasMensais? obterMetricas(String uf, int ano, int mes){
-
-    }
-
+  MetricasMensais? obterMetricas(
+    String uf,
+    int ano,
+    int mes,
+  ) {
+    return _dados[uf.toUpperCase()]?[ano]?[mes];
+  }
 }
